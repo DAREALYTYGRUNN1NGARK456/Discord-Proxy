7b889be944c023cb6289353ae1210804543fcd09
!Glitched! <51778028+DaKing01@users.noreply.github.com>
2022-06-20 18:31:08 +1000

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ global.logger = winston.createLogger({
     transports: [
       new winston.transports.File({ filename: 'error.log', level: 'error' }),
       new winston.transports.File({ filename: 'combined.log' }),
-      new winston.transports.Console()
+      //new winston.transports.Console()
     ],
 });
 
